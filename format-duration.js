87b68850245782da548f9d05ad3804bf48c8d493
Ryan <ryan.o.mackey@gmail.com>
2016-10-25 14:04:29 -0600

    finished time formatter

--- format-duration.js
+++ format-duration.js
@@ -1,19 +1,28 @@
 'use strict';
 
 function formatDuration(seconds) {
+  var formatted;
   if (seconds === 0) {
     return 'now';
   } else if (seconds > 0 && seconds < 60) {
-    return timeParser(seconds, 'second');
+    formatted = timeParser(seconds, 'second');
   } else if (seconds >= 60 && seconds < 3600) {
-    return minuteParser(seconds);
+    formatted = minuteParser(seconds);
   } else if (seconds >= 3600 && seconds < 86400) {
-    return hourParser(seconds);
+    formatted = hourParser(seconds);
   } else if (seconds >= 86400 && seconds < 31536000) {
-    return dayParser(seconds);
+    formatted = dayParser(seconds);
   } else {
-    return yearParser(seconds);
+    formatted = yearParser(seconds);
   }
+  return addAnd(formatted);
+}
+
+function addAnd(str) {
+  var toArr = str.split('');
+  var index = toArr.lastIndexOf(',');
+  toArr[index] = ' and';
+  return toArr.join('');
 }
 
 function yearParser(seconds) {
@@ -49,7 +58,7 @@ function hourParser(seconds) {
 function minuteParser(seconds) {
   var remainder = seconds % 60;
   if (remainder) {
-    return timeParser(Math.floor(seconds/60), 'minute') + ' and ' + timeParser(seconds % 60, 'second');
+    return timeParser(Math.floor(seconds/60), 'minute') + ', ' + timeParser(seconds % 60, 'second');
   } else {
     return timeParser(Math.floor(seconds/60), 'minute');
   }
@@ -70,9 +79,3 @@ console.log(formatDuration(3600)); // '1 hour'
 console.log(formatDuration(3662)); // '1 hour, 1 minute and 2 seconds'
 console.log(formatDuration(15731080)); // '182 days, 1 hour, 44 minutes and 40 seconds'
 console.log(formatDuration(132030240)); // '4 years, 68 days, 3 hours and 4 minutes'
-
-// Test.assertEquals(formatDuration(1), "1 second");
-// Test.assertEquals(formatDuration(62), "1 minute and 2 seconds");
-// Test.assertEquals(formatDuration(120), "2 minutes");
-// Test.assertEquals(formatDuration(3600), "1 hour");
-// Test.assertEquals(formatDuration(3662), "1 hour, 1 minute and 2 seconds");
